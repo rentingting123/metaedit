87696df978140576141441076511d73d2c7df4af
rentingting <1542078062@qq.com>
2023-12-20 09:58:24 +0800

    拖拽

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
+<!--
+ * @Author: rentingting 1542078062@qq.com
+ * @Date: 2023-12-11 20:54:17
+ * @LastEditors: rentingting 1542078062@qq.com
+ * @LastEditTime: 2023-12-20 09:32:28
+ * @FilePath: /code/metaedit/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <router-view />
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App'
-})
+  name: "App",
+});
 </script>
+<style>
+html,
+body {
+  -webkit-app-region: drag;
+}
+</style>
