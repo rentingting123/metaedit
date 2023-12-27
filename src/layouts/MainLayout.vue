c5e53b6c82ded21f435738df78c1cfabc3852edd
rentingting <1542078062@qq.com>
2023-12-27 22:29:55 +0800

    场景和图层页面开发

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -62,7 +62,32 @@
         <q-btn flat color="primary" label="运行" @click="handleRun" />
         <q-btn flat color="primary" label="编译" @click="handleCompile" />
         <q-btn flat color="primary" label="横屏" @click="handleLandscape" />
-        <q-btn flat color="primary" label="显示比例" @click="handleScale" />
+        <div>
+          <q-btn flat color="primary" label="显示比例" @click="handleScale" />
+          <q-menu
+            auto-close
+            dark
+            transition-show="flip-right"
+            transition-hide="flip-left"
+          >
+            <q-list dense style="min-width: 100px">
+              <q-item clickable>
+                <q-item-section>4:3 </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>16:9</q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>3:2</q-item-section>
+              </q-item>
+              <q-separator />
+              <q-item clickable>
+                <q-item-section>自定义</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </div>
+
         <q-space />
         <q-btn dense flat icon="minimize" @click="handleMainEvent('min')" />
         <q-btn dense flat icon="crop_square" @click="handleMainEvent('max')" />
@@ -122,13 +147,112 @@
                 </q-input>
               </div>
             </div>
+            <!-- 场景列表 -->
             <div class="side-list">
               <div
                 class="side-item color-tx1"
                 v-for="(item, index) in sceneList"
                 :key="index"
+                :class="{ active: sceneIndex === index }"
+                @click="handleSceneClick(index)"
               >
-                {{ item.name }}
+                <span>
+                  <q-icon name="bi-layout-wtf" class="iconstyle" />
+                  {{ item.name }}
+                </span>
+
+                <div v-if="sceneIndex === index">
+                  <q-icon
+                    name="bi-trash3"
+                    class="iconstyle"
+                    v-if="sceneList.length > 1"
+                    @click="deleteScene(index)"
+                  />
+                  <span>
+                    <q-icon name="bi-plus" />
+                    <q-menu
+                      auto-close
+                      dark
+                      transition-show="flip-right"
+                      transition-hide="flip-left"
+                    >
+                      <q-list dense style="min-width: 100px">
+                        <q-item clickable @click="handleAddSceneType(1, index)">
+                          <q-item-section>
+                            <div>
+                              <q-icon
+                                name="bi-camera-video"
+                                class="iconstyle"
+                              />视频
+                            </div>
+                          </q-item-section>
+                        </q-item>
+                        <q-item clickable @click="handleAddSceneType(2, index)">
+                          <q-item-section>
+                            <div>
+                              <q-icon
+                                name="bi-cursor-text"
+                                class="iconstyle"
+                              />序列帧
+                            </div></q-item-section
+                          >
+                        </q-item>
+                        <q-separator />
+                        <q-item clickable @click="handleAddSceneType(3, index)">
+                          <q-item-section>
+                            <div>
+                              <q-icon
+                                name="bi-card-image"
+                                class="iconstyle"
+                              />图片
+                            </div>
+                          </q-item-section>
+                        </q-item>
+                        <q-item clickable @click="handleAddSceneType(4, index)">
+                          <q-item-section
+                            ><div>
+                              <q-icon
+                                name="bi-chat-dots"
+                                class="iconstyle"
+                              />对话
+                            </div></q-item-section
+                          >
+                        </q-item>
+                        <q-item clickable @click="handleAddSceneType(5, index)">
+                          <q-item-section
+                            ><div>
+                              <q-icon
+                                name="bi-stop-fill"
+                                class="iconstyle"
+                              />按钮
+                            </div></q-item-section
+                          >
+                        </q-item>
+                        <q-item clickable @click="handleAddSceneType(6, index)">
+                          <q-item-section
+                            ><div>
+                              <q-icon
+                                name="bi-stop-btn"
+                                class="iconstyle"
+                              />按钮组
+                            </div></q-item-section
+                          >
+                        </q-item>
+                        <q-separator />
+                        <q-item clickable @click="handleAddSceneType(7, index)">
+                          <q-item-section
+                            ><div>
+                              <q-icon
+                                name="bi-suit-diamond"
+                                class="iconstyle"
+                              />几何
+                            </div></q-item-section
+                          >
+                        </q-item>
+                      </q-list>
+                    </q-menu>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
@@ -172,12 +296,37 @@
               </div>
             </div>
             <div class="side-list">
+              <!-- 图层列表 -->
               <div
                 class="side-item color-tx1"
-                v-for="(item, index) in levelList"
+                v-for="(item, index) in sceneList[sceneIndex].levelList"
                 :key="index"
+                :class="{ active: levelIndex === index }"
+                @click="handleLevelClick(item.type, index)"
               >
-                {{ item.name }}
+                <span>
+                  <q-icon name="bi-camera-video" v-if="item.type == 1" />
+                  <q-icon name="bi-cursor-text" v-if="item.type == 2" />
+                  <q-icon name="bi-card-image" v-if="item.type == 3" />
+                  <q-icon name="bi-chat-dots" v-if="item.type == 4" />
+                  <q-icon name="bi-stop-fill" v-if="item.type == 5" />
+                  <q-icon name="bi-stop-btn" v-if="item.type == 6" />
+                  <q-icon name="bi-suit-diamond" v-if="item.type == 7" />
+                  {{ item.name }}</span
+                >
+                <div v-if="item.isShow">
+                  <q-icon
+                    name="bi-trash3"
+                    @click="deleteLevel(index)"
+                    class="iconstyle"
+                  />
+                  <q-icon name="bi-eye" @click="item.isShow = false" />
+                </div>
+                <q-icon
+                  v-else
+                  name="bi-eye-slash"
+                  @click="item.isShow = true"
+                />
               </div>
             </div>
           </div>
@@ -192,7 +341,13 @@
       class="color-bg2"
       side="right"
     >
-      <RightForm></RightForm>
+      <RightForm1 v-if="curType == 1"></RightForm1>
+      <RightForm2 v-if="curType == 2"></RightForm2>
+      <RightForm3 v-if="curType == 3"></RightForm3>
+      <RightForm4 v-if="curType == 4"></RightForm4>
+      <RightForm5 v-if="curType == 5"></RightForm5>
+      <RightForm6 v-if="curType == 6"></RightForm6>
+      <RightForm7 v-if="curType == 7"></RightForm7>
     </q-drawer>
     <q-page-container>
       <router-view />
@@ -203,7 +358,13 @@
 <script setup >
 import { ref } from "vue";
 const { ipcRenderer } = require("electron");
-import RightForm from "./RightForm.vue";
+import RightForm1 from "./RightForm1.vue";
+import RightForm2 from "./RightForm2.vue";
+import RightForm3 from "./RightForm3.vue";
+import RightForm4 from "./RightForm4.vue";
+import RightForm5 from "./RightForm5.vue";
+import RightForm6 from "./RightForm6.vue";
+import RightForm7 from "./RightForm7.vue";
 
 //组件注册
 
@@ -217,18 +378,57 @@ const splitterModel = ref(50); // start at 50%
 const sceneText = ref("");
 const showSearchScene = ref(false); //场景
 const showSearchLevel = ref(false); //图层
+const sceneIndex = ref(0); //场景当前选中
+const levelIndex = ref(0); //图层当前选中
+const curType = ref(1); //类型当前选中
 const sceneList = ref([
   {
     id: 1,
     name: "场景1",
-  },
-  {
-    id: 2,
-    name: "场景2",
-  },
-  {
-    id: 3,
-    name: "场景3",
+    levelList: [
+      {
+        id: new Date().getTime(), //获取当前时间戳
+        type: 1,
+        name: "视频",
+        isShow: true,
+      },
+      {
+        id: new Date().getTime(), //获取当前时间戳
+        type: 2,
+        name: "序列帧",
+        isShow: true,
+      },
+      {
+        id: new Date().getTime(), //获取当前时间戳
+        type: 3,
+        name: "图片",
+        isShow: true,
+      },
+      {
+        id: new Date().getTime(), //获取当前时间戳
+        type: 4,
+        name: "对话",
+        isShow: true,
+      },
+      {
+        id: new Date().getTime(), //获取当前时间戳
+        type: 5,
+        name: "按钮",
+        isShow: true,
+      },
+      {
+        id: new Date().getTime(), //获取当前时间戳
+        type: 6,
+        name: "按钮组",
+        isShow: true,
+      },
+      {
+        id: new Date().getTime(), //获取当前时间戳
+        type: 7,
+        name: "几何",
+        isShow: true,
+      },
+    ], //图层
   },
 ]);
 // 视频
@@ -237,46 +437,17 @@ const sceneList = ref([
 // 对话
 // 按钮
 // 按钮组
-const levelType = ref([
-  {
-    id: 1,
-    name: "视频",
-  },
-  {
-    id: 2,
-    name: "序列帧",
-  },
-  {
-    id: 3,
-    name: "图片",
-  },
-  {
-    id: 4,
-    name: "对话",
-  },
-  {
-    id: 5,
-    name: "按钮",
-  },
-  {
-    id: 5,
-    name: "按钮组",
-  },
-]);
-const levelList = ref([
-  {
-    id: 1,
-    name: "视频",
-  },
-  {
-    id: 2,
-    name: "图片",
-  },
-  {
-    id: 3,
-    name: "按钮",
-  },
-]);
+// 几何
+const levelType = ref({
+  1: "视频",
+  2: "序列帧",
+  3: "图片",
+  4: "对话",
+  5: "按钮",
+  6: "按钮组",
+  7: "几何",
+});
+
 const leftDrawerOpen = ref(false);
 const rightDrawerOpen = ref(true);
 const toggleLeftDrawer = (eventName) => {
@@ -303,8 +474,9 @@ const addScene = () => {
   console.log("addScene");
   //给sceneList 追加
   sceneList.value.push({
-    id: sceneList.value.length + 1,
+    id: new Date().getTime(), //获取当前时间戳
     name: "场景" + (sceneList.value.length + 1),
+    levelList: [], //图层
   });
 };
 //搜索场景
@@ -315,6 +487,33 @@ const searchScene = () => {
 const searchLevel = () => {
   showSearchLevel.value = !showSearchLevel.value;
 };
+// 一级场景点击
+const handleSceneClick = (index) => {
+  sceneIndex.value = index;
+};
+//场景删除
+const deleteScene = (index) => {
+  sceneList.value.splice(index, 1);
+};
+//添加场景类型
+const handleAddSceneType = (type, index) => {
+  //给sceneList[index].levelList 追加
+  sceneList.value[index].levelList.push({
+    id: new Date().getTime(), //获取当前时间戳
+    type: type,
+    name: levelType.value[type] + (sceneList.value[index].levelList.length + 1),
+    isShow: true,
+  });
+};
+// 二级 图层点击
+const handleLevelClick = (type, index) => {
+  levelIndex.value = index;
+  curType.value = type;
+};
+//二级 层级删除
+const deleteLevel = (index) => {
+  sceneList.value[sceneIndex.value].levelList.splice(index, 1);
+};
 </script>
 <style scoped>
 .side-list {
@@ -324,6 +523,14 @@ const searchLevel = () => {
   text-align: left;
   width: 100%;
   padding: 6px 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.active {
+  background: #333;
+  color: #fff;
 }
 .scene {
   display: flex;
@@ -338,5 +545,8 @@ const searchLevel = () => {
 .search-ipt {
   height: 20px;
   margin-left: 20px;
+}
+.iconstyle {
+  margin-right: 6px;
 }
 </style>
---
+++ src/layouts/RightForm4.vue
@@ -0,0 +1,116 @@
+<!--
+ * @Author: rentingting 1542078062@qq.com
+ * @Date: 2023-12-20 13:45:53
+ * @LastEditors: rentingting 1542078062@qq.com
+ * @LastEditTime: 2023-12-27 22:20:26
+ * @FilePath: /code/metaedit/src/layouts/RightForm.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+    <!-- 公共属性-->
+    <!-- 视频 -->
+    <div class="scene color-bg2">
+      <div class="color-tx1">属性</div>
+    </div>
+    <div class="attribute">
+      <!-- 视频属性 -->
+      <el-form label-position="right" label-width="60px" :model="formValue1">
+        <div class="card">
+          <div class="card-header">
+            <span class="color-tx1">基础</span>
+          </div>
+          <el-form-item label="类型">
+            <div>{{ formValue1.public.typeName }}</div>
+          </el-form-item>
+        </div>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref } from "vue";
+//视频
+const formValue1 = ref({
+  // 变换：xy size
+  // 显示隐藏
+  public: {
+    typeId: 1,
+    typeName: "对话", //   类型（字符串）：固定值，不可编辑
+    id: "", // id（字符串）：可以更改，用作写脚本时查找组件。
+    name: "", // 名称（字符串）：可以更改，用作左侧大纲显示名称。
+    size: {
+      //变换：xy size
+      x: 1,
+      y: 1,
+      w: 10,
+      h: 10,
+    },
+    hide: true, //显示隐藏
+  },
+  video: {
+    path: "", //路径（文件选择）：显示文件路径，点击按钮可以重新选择。
+    loop: false, //循环（复选框）：勾选上，视频会自动循环。
+    autoplay: false, //自动播放（复选框）：勾选上后，视频会自动播放。
+    tileId: 1,
+    tileType: [
+      {
+        value: 1,
+        label: "拉伸",
+      },
+      {
+        value: 2,
+        label: "满铺",
+      },
+      {
+        value: 3,
+        label: "居中",
+      },
+    ], //平铺类型 可选项：拉伸、满铺、居中。
+  },
+  layout: {
+    id: 1,
+    up: 1,
+    down: 1,
+    left: 1,
+    right: 1,
+  },
+});
+</script>
+
+<style lang="scss">
+.scene {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  border-bottom: 1px solid #000;
+  /* 固定在顶部栏 */
+  position: fixed;
+  width: 100%;
+}
+.attribute {
+  padding: 40px 0;
+  color: #fff;
+  .card {
+    border-bottom: 1px solid #333;
+    padding: 0 10px;
+  }
+  .el-card__header {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 30px;
+    padding: 20px;
+  }
+  .lable {
+    margin-right: 6px;
+    margin-left: 6px;
+  }
+}
+</style>
